8f11160e6fa8dd1c447c723725d4e98f81ffe5fb
JR Rickerson <jrrickerson@redrivetstudios.com>
2021-12-02 09:20:49 -0500

    Do a quick refactor on Day 1 solution

--- python/day-01/solve.py
+++ python/day-01/solve.py
@@ -1,33 +1,4 @@
-
-
-def sliding_windows(measurement_list, window=3):
-    sums = []
-    if len(measurement_list) < window:
-        return sums
-    for i in range(0, len(measurement_list)):
-        window_list = measurement_list[i:i+window]
-        if len(window_list) < window:
-            break
-        window_sum = sum(window_list)
-        sums.append(window_sum)
-    return sums
-
-
-def load_depth_list(file_obj):
-    return [int(line.strip()) for line in file_obj if line.strip()]
-
-
-def count_increases(value_list):
-    if not value_list:
-        return 0
-
-    previous = None
-    count = 0
-    for item in value_list:
-        if previous is not None and previous < item:
-            count += 1
-        previous = item
-    return count
+from utils import load_depth_list, count_increases, sliding_windows
 
 
 def part_1(input_file):
